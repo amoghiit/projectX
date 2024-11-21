95b8114eba9cb13c5852a13961eab440932c5825
rakeshreddy06 <rakeshreddy5566@gmail.com>
2024-11-20 22:35:55 -0500

    z

--- src/model_selection.py
+++ src/model_selection.py
@@ -13,7 +13,7 @@ class ModelSelection:
         return np.mean((y_true - y_pred) ** 2)
 
     def r_squared(self, y_true, y_pred):
-        ss_total = np.sum((y_true - np.mean(y_true)) ** 2)
+        ss_total = np.sum((y_true - np.mean(y_true)) ** 2) 
         ss_residual = np.sum((y_true - y_pred) ** 2)
         return 1 - (ss_residual / ss_total)
 
